b1f7deccf6afa737c292ef19fca4294ec062e3b7
kzeljko <karlo.zeljko@hotmail.com>
2025-02-25 09:33:58 +0000

    created uart_log function

--- congateway/software_uart.c
+++ congateway/software_uart.c
@@ -7,7 +7,7 @@ void uart_tx_init(void) {
     nrf_gpio_cfg_output(UART_TX_PIN);
 }
 
-void uart_tx_byte(uint8_t byte, uint32_t bit_delay) {
+static void uart_tx_byte(uint8_t byte) {
     // Start bit (low)
     nrf_gpio_pin_clear(UART_TX_PIN);
     nrf_delay_us(BIT_DELAY); // Bit delay for 1 bit duration
@@ -28,9 +28,18 @@ void uart_tx_byte(uint8_t byte, uint32_t bit_delay) {
     nrf_delay_us(BIT_DELAY); // Wait for the stop bit duration
 }
 
-// Transmit a buffer
-void uart_tx_buffer(const uint8_t* buffer, size_t size, uint32_t bit_delay) {
+static void uart_tx_buffer(const uint8_t* buffer, size_t size) {
     for (size_t i = 0; i < size; i++) {
-        uart_tx_byte(buffer[i], bit_delay);  // Transmit each byte in the buffer
+        uart_tx_byte(buffer[i]);
     }
+}
+
+void uart_log(const char *format, ...){
+    char log_buffer[100] = {0};
+    va_list args;
+    va_start(args, format);
+    int log_len = vsnprintf(log_buffer, 100, format, args);
+    va_end(args);
+
+    uart_tx_buffer((uint8_t *)log_buffer, log_len);
 }
--- congateway/software_uart.h
+++ congateway/software_uart.h
@@ -1,9 +1,12 @@
 #include "nrf_gpio.h"
 #include "nrf_delay.h"
+#include <stdarg.h>
 
 #define UART_TX_PIN    30
-#define BIT_DELAY      200  // Bit delay in microseconds (adjust as needed for your baud rate)
+#define BAUD_RATE 4800
+#define BIT_DELAY (1000000 / BAUD_RATE)
 
 void uart_tx_init(void);
-void uart_tx_byte(uint8_t byte, uint32_t bit_delay);
-void uart_tx_buffer(const uint8_t* buffer, size_t size, uint32_t bit_delay);
+static void uart_tx_byte(uint8_t byte);
+static void uart_tx_buffer(const uint8_t* buffer, size_t size);
+void uart_log(const char *format, ...);
